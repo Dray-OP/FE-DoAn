609c8ea80157faf032db738c958f1ec801f5fe12
Dray-OP <holongnhat2000@gmail.com>
2020-03-24 11:53:49 +0700

    update database

---
+++ quanlyAccountWebsite.sql
@@ -0,0 +1,12 @@
+﻿create database quanlyAccountWebsite
+go
+
+use quanlyAccountWebsite
+go
+
+create table Account(
+	UserName nvarchar(1000) primary key,
+	DisplayName Nvarchar(1000) not null default N'DrayOP',
+	PassWord Nvarchar(1000) not null default 0, -- mã hóa nên để dài ra
+	Type int not null default 0 --kiểu dữ liệu
+)
